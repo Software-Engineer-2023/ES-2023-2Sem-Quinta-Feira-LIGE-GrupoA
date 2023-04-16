6400b6fb9f9c2c32337b159f6ae6e08f507250ff
RafaelGesero <racgo@iscte-iul.pt>
2023-04-16 13:17:48 +0100

    Inicio do projeto

---
+++ src/main/java/ES_Projeto_GrupoA_2023/projetoES/App.java
@@ -0,0 +1,13 @@
+package ES_Projeto_GrupoA_2023.projetoES;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
