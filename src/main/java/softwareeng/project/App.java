32e050791efc9736b8cb287eeafc8da418339428
IronIdk <jmsfo@iscte-iul.pt>
2023-04-27 14:07:34 +0100

    update/App.java
    Optimized App.java

--- src/main/java/softwareeng/project/App.java
+++ src/main/java/softwareeng/project/App.java
@@ -1,10 +1,9 @@
 package softwareeng.project;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class App {
     private static final Logger LOGGER = Logger.getLogger(App.class.getName());
@@ -21,34 +20,18 @@ public class App {
             mainMenu.getConvertSchedulesButton().addActionListener(e -> {
                 String location = "webcal://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=rageo@iscte.pt&password=zLgFKoKyGjZf1Ago80qtjmy8f0eS5uDCJQZSq2MNDGbZlTcMLw7pXDjThCYU52bDlIZsBYjNgXsIGLGUYPs8HHDfk9YnHQIZtkZXHgyBlk1nvaoTbqw4S2BG4V70CcTl";
 
-                if(location.startsWith("webcal")) {
                     Web web = new Web();
-                    String s = location.replace("webcal", "https");
+                        String s = location.replace("webcal", "https");
 
-                    try {
-                        URL url = new URL(s);
-                        web.ReadWeb(url);
-                        web.URLToCSV(url);
-                        web.URLToJson(url);
-                    } catch (MalformedURLException ex) {
-                        LOGGER.log(Level.SEVERE, "Exception occurred", ex);
-                    } catch (IOException ex) {
-                        LOGGER.log(Level.SEVERE, "Exception occurred", ex);
+                        try {
+                            URL url = new URL(s);
+                            web.ReadWeb(url);
+                            web.URLToCSV(url);
+                            web.URLToJson(url);
+                        } catch (IOException ex) {
+                            LOGGER.log(Level.SEVERE, "Exception occurred", ex);
+                        }
                     }
-                } else {
-                    Web web = new Web();
-
-                    try {
-                        URL url = new URL(location);
-                        web.ReadWeb(url);
-                        web.URLToCSV(url);
-                    } catch (MalformedURLException ex) {
-                        LOGGER.log(Level.SEVERE, "Exception occurred", ex);
-                    } catch (IOException ex) {
-                        LOGGER.log(Level.SEVERE, "Exception occurred", ex);
-                    }
-                }
-            }
             );
 
 
