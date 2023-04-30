32c4a6fc20808ec84ef5c5357780b605d28a1eca
Rafael Etelvino <rageo@iscte-iul.pt>
2023-04-30 11:11:26 +0100

    created/WebTest classe de testes da classe web

--- src/test/java/ES_Projeto_GrupoA_2023/projetoES/WebTest.java
+++ src/test/java/ES_Projeto_GrupoA_2023/projetoES/WebTest.java
@@ -1,23 +1,92 @@
 package ES_Projeto_GrupoA_2023.projetoES;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import softwareeng.project.Web;
 
-import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class WebTest {
+ class WebTest {
 
     @Test
-    void readWeb() {
+    void testReadWebWithValidUrl() {
+        Web web = new Web();
+        URL url;
+        try {
+            url = new URL("https://www.example.com");
+            Assertions.assertDoesNotThrow(() -> web.ReadWeb(url));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    void URLToCSV() {
+    void testReadWebWithInvalidUrl() {
+        Web web = new Web();
+        URL url;
+        try {
+            url = new URL("invalidurl");
+            Exception exception = Assertions.assertThrows(Exception.class, () -> web.ReadWeb(url));
+            Assertions.assertEquals("URL inválido: no protocol: invalidurl", exception.getMessage());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    void testURLToCSVWithInvalidUrl() {
+        Web web = new Web();
+        URL url;
+        try {
+            url = new URL("invalidurl");
+            Exception exception = Assertions.assertThrows(Exception.class, () -> web.URLToCSV(url));
+            Assertions.assertEquals("unknown protocol: invalidurl", exception.getMessage());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    @Test
+    void testURLToJsonWithInvalidUrl() {
+        Web web = new Web();
+        URL url;
+        try {
+            url = new URL("invalidurl");
+            Exception exception = Assertions.assertThrows(Exception.class, () -> web.URLToJson(url));
+            Assertions.assertEquals("unknown protocol: invalidurl", exception.getMessage());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    void URLToJson() {
+    void testDownloadWebContentWithValidUrlContainingHTML() {
+        Web web = new Web();
+        URL url;
+        try {
+            url = new URL("https://example.com");
+            Assertions.assertDoesNotThrow(() -> web.downloadWebContent(url));
+            // TODO: Add assertions to verify if HTML content was correctly written to file
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
+
+    @Test
+    void testDownloadWebContentWithInvalidUrl() {
+        Web web = new Web();
+        URL url;
+        try {
+            url = new URL("invalidurl");
+            Exception exception = Assertions.assertThrows(Exception.class, () -> web.downloadWebContent(url));
+            Assertions.assertEquals("unknown protocol: invalidurl", exception.getMessage());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
